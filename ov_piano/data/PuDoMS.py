184d3e298e8ef202ce1e8b3a093c0b75d42585b1
nfriche <111462149+nfriche@users.noreply.github.com>
2024-02-14 18:41:23 +0100

    Update PuDoMS.py with class for full dataset

--- ov_piano/data/PuDoMS.py
+++ ov_piano/data/PuDoMS.py
@@ -64,6 +64,57 @@ class MONO_pudoms:
         path = os.path.join(self.rootpath, matches[0])
         return path
 
+class PuDoMS:
+    """
+    This class parses the filesystem tree for the PuDoMS dataset and, based on
+    the given filters, stores a list of file paths.
+
+    It can be used to manage PuDoMS files and to create custom dataloaders.
+    """
+
+    CSV_NAME = "pudoms.csv"
+    ALL_SPLITS = {"train", "validation", "test"}
+    AUDIO_EXT = ".wav"
+    MIDI_EXTS = [".midi", ".mid"]
+
+    def __init__(self, rootpath, splits=None):
+        """
+        """
+        self.rootpath = rootpath
+        self.meta_path = os.path.join(rootpath, self.CSV_NAME)
+        # filter sanity check
+        if splits is None:
+            splits = self.ALL_SPLITS
+        assert (s in self.ALL_SPLITS for s in splits), \
+            f"Unknown split in {splits}"
+        # load and filter csv
+        df = pd.read_csv(self.meta_path)
+        df = df[df["Split"].isin(splits)]
+        # reformat into DATA_COLUMNS + metadata_str and gather
+        columns = ["File_Number", "Split", "Duration",
+                   "Composer", "Title"]
+        self.data = []
+        for i, (id, s, dur, comp, title) in df[columns].iterrows():
+            meta = (s, dur, comp, title)  
+            self.data.append((str(id), meta))
+        self.full_data = df
+
+    def get_file_abspath(self, basename):
+        """
+        :param basename: Base name of the corresponding MIDI file without
+         extension, e.g.
+         MIDI-Unprocessed_R1_D1-1-8_mid--AUDIO-from_mp3_08_R1_2015_wav--4'
+        :returns: Unique absolute path for that basename
+        """
+        for ext in self.MIDI_EXTS:
+            # Attempt to find a matching file with each supported extension
+            matches = [fn for fn, _ in self.data if basename in fn]
+            for match in matches:
+                midi_path = os.path.join(self.rootpath, match + ext)
+                if os.path.exists(midi_path):
+                    return midi_path
+        raise FileNotFoundError(f"No matching file found for basename: {basename}")
+
 
 # ##############################################################################
 # #  PYTORCH DATASETS
