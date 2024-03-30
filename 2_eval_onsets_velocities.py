4b9fb9d87c0895f9cb033bc1b7eeb3037bfac17d
nfriche <111462149+nfriche@users.noreply.github.com>
2024-03-30 16:11:40 +0100

    Update 2_eval_onsets_velocities.py

--- 2_eval_onsets_velocities.py
+++ 2_eval_onsets_velocities.py
@@ -109,7 +109,7 @@ class ConfDef:
         "MAESTROv3_roll_quant=0.024_midivals=128_extendsus=True.h5")
     SNAPSHOT_INPATH: str = os.path.join("out",
                                         "model_snapshots",
-                                        "OnsetsAndVelocities_2024_03_16_03_48_26.846.torch")
+                                        "OnsetsAndVelocities_2024_03_26_15_06_49.015__step=71000_f1=0.9621__0.9353.torch")
     #
     CONV1X1: List[int] = (200, 200)
     LEAKY_RELU_SLOPE: Optional[float] = 0.1
@@ -319,5 +319,5 @@ if __name__ == "__main__":
     results_folder = "results"
     os.makedirs(results_folder, exist_ok=True)
     
-    test_results_df.to_csv(os.path.join(results_folder, 'full_pud_test_results.csv'), index=False)
-    test_results_df_vel.to_csv(os.path.join(results_folder, 'full_pud_test_results_vel.csv'), index=False)
+    test_results_df.to_csv(os.path.join(results_folder, 'full_pud_test_results_5.csv'), index=False)
+    test_results_df_vel.to_csv(os.path.join(results_folder, 'full_pud_test_results_5_vel.csv'), index=False)
